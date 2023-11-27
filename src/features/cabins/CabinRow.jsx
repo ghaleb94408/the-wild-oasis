396b507af0bf2aa15ff19d435cd4efa59529cded
Ghaleb Murabyeh <ghaleb.murapyeh@gmail.com>
2023-11-27 16:12:20 +0300

    add delete cabin function

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -1,5 +1,7 @@
 import styled from "styled-components";
 import { formatCurrency } from "../../utils/helpers";
+import { deleteCabin } from "../../services/apiCabins";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 const TableRow = styled.div`
   display: grid;
   grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
@@ -38,9 +40,26 @@ const Discount = styled.div`
   font-weight: 500;
   color: var(--color-green-700);
 `;
-
 export default function CabinRow({ cabin }) {
-  const { name, maxCapacity, regularPrice, discount, image } = cabin;
+  const {
+    name,
+    maxCapacity,
+    regularPrice,
+    discount,
+    image,
+    id: cabinId,
+  } = cabin;
+  const queryClient = useQueryClient();
+  const { isPending, mutate } = useMutation({
+    mutationFn: deleteCabin, //same as (id)=>dleteCabin(id)
+    onSuccess: () => {
+      alert("Cabin Successfully Deleted");
+      queryClient.invalidateQueries({
+        queryKey: ["cabins"],
+      });
+    },
+    onError: (err) => alert(err.message),
+  });
   return (
     <TableRow role="row">
       <Img src={image} />
@@ -48,7 +67,9 @@ export default function CabinRow({ cabin }) {
       <div>Fits up to {maxCapacity}</div>
       <Price>{formatCurrency(regularPrice)}</Price>
       <Discount>{formatCurrency(discount)}</Discount>
-      <button>Delete</button>
+      <button onClick={() => mutate(cabinId)} disabled={isPending}>
+        Delete
+      </button>
     </TableRow>
   );
 }
--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -6,6 +6,14 @@ export async function getCabins() {
     console.error(error);
     throw new Error("Cabins could not be loaded");
   }
-  
+
   return data;
+}
+export async function deleteCabin(id) {
+  const { error } = await supabase.from("cabins").delete().eq("id", id);
+  if (error) {
+    console.error(error);
+    throw new Error("Cabin could not be deleted");
+  }
+  return error;
 }
