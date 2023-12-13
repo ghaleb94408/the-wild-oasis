90756b96d35b2d8ba4def2ac361b59ee8e002876
Ghaleb Murabyeh <ghaleb.murapyeh@gmail.com>
2023-12-13 17:23:18 +0300

    add settings page data fetching and updating

---
+++ src/features/settings/useUpdateSettings.js
@@ -0,0 +1,18 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { updateSetting as updateSettingsApi } from "../../services/apiSettings";
+import toast from "react-hot-toast";
+
+export default function useUpdateSettings() {
+  const queryClient = useQueryClient();
+  const { isPending: isUpdating, mutate: updateSettings } = useMutation({
+    mutationFn: updateSettingsApi,
+    onSuccess: () => {
+      toast.success("Settings successfully edited");
+      queryClient.invalidateQueries({ queryKey: ["settings"] });
+    },
+    onError: (err) => {
+      toast.error(err.message);
+    },
+  });
+  return { isUpdating, updateSettings };
+}
