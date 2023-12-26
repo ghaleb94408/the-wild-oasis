9e3c9c8c83a1f3a025b2b87168aa2a3e330ab662
Ghaleb Murabyeh <ghaleb.murapyeh@gmail.com>
2023-12-27 01:04:31 +0300

    create cabinTableOperation for filtering and sorting and add filter functionality

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -3,10 +3,20 @@ import CabinRow from "./CabinRow";
 import useCabins from "./useCabins";
 import Table from "../../ui/Table";
 import Menus from "../../ui/Menus";
+import { useSearchParams } from "react-router-dom";
 
 export default function CabinTable() {
   const { cabins, isLoading } = useCabins();
+  const [searchParams] = useSearchParams();
   if (isLoading) return <Spinner />;
+
+  const filterValue = searchParams.get("discount") || "all";
+  let filteredCabins;
+  if (filterValue === "all") filteredCabins = cabins;
+  else if (filterValue === "no-discount")
+    filteredCabins = cabins.filter((cabin) => cabin.discount === 0);
+  else if (filterValue === "with-discount")
+    filteredCabins = cabins.filter((cabin) => cabin.discount > 0);
   return (
     <Menus>
       <Table $columns="0.6fr 1.8fr 2.2fr 1fr 1fr 1fr">
@@ -19,7 +29,7 @@ export default function CabinTable() {
           <div></div>
         </Table.Header>
         <Table.Body
-          data={cabins}
+          data={filteredCabins}
           render={(cabin) => <CabinRow cabin={cabin} key={cabin.id} />}
         />
       </Table>
--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -3,12 +3,13 @@ import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 
 import AddCabin from "../features/cabins/AddCabin";
+import CabinTableOperations from "../features/cabins/CabinTableOperations";
 function Cabins() {
   return (
     <>
       <Row type="horizontal">
         <Heading as="h1">All cabins</Heading>
-        <p>Filter / sort</p>
+        <CabinTableOperations />
       </Row>
       <Row>
         <CabinTable />
