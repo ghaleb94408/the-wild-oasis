56e6d4d2bf6d8aedbc2c88f5e8787668c727597f
Ghaleb Murabyeh <ghaleb.murapyeh@gmail.com>
2024-01-05 22:06:58 +0300

    add authorization for protected routes

--- src/App.jsx
+++ src/App.jsx
@@ -14,11 +14,12 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Toaster } from "react-hot-toast";
 import Checkin from "./pages/Checkin";
+import ProtectedRoute from "./ui/ProtectedRoute";
 
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      staleTime: 10 * 1000,
+      staleTime: 5 * 60000,
     },
   },
 });
@@ -29,7 +30,13 @@ function App() {
       <GlobalStyles />
       <BrowserRouter>
         <Routes>
-          <Route element={<AppLayout />}>
+          <Route
+            element={
+              <ProtectedRoute>
+                <AppLayout />
+              </ProtectedRoute>
+            }
+          >
             <Route index element={<Navigate replace to={"/dashboard"} />} />
             <Route path="/dashboard" element={<Dashboard />} />
             <Route path="/bookings" element={<Bookings />} />
--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -1,12 +1,15 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { login as loginApi } from "../../services/apiAuth";
 import { useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
 export function useLogin() {
+  const queryClient = useQueryClient();
   const navigate = useNavigate();
   const { mutate: login, isPending: isLoggingin } = useMutation({
     mutationFn: (credentials) => loginApi(credentials),
-    onSuccess: () => {
+    onSuccess: (user) => {
+      console.log(user.user);
+      queryClient.setQueryData(["user"], user.user);
       navigate("/dashboard");
     },
     onError: (err) => {
