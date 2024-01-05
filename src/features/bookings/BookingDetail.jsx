cdfa6ab7b8f1c629126b7825eeacdc7edfd592f6
Ghaleb Murabyeh <ghaleb.murapyeh@gmail.com>
2024-01-05 16:16:41 +0300

    add checkout functionality to bookings

--- src/features/bookings/BookingDetail.jsx
+++ src/features/bookings/BookingDetail.jsx
@@ -12,6 +12,7 @@ import ButtonText from "../../ui/ButtonText";
 import { useMoveBack } from "../../hooks/useMoveBack";
 import { useBooking } from "./useBooking";
 import { useNavigate } from "react-router-dom";
+import { useCheckout } from "../check-in-out/useCheckout";
 
 const HeadingGroup = styled.div`
   display: flex;
@@ -22,7 +23,7 @@ const HeadingGroup = styled.div`
 function BookingDetail() {
   const navigate = useNavigate();
   const { booking, isLoading } = useBooking();
-
+  const { checkout, isCheckingout } = useCheckout();
   const moveBack = useMoveBack();
 
   if (isLoading) return <Spinner />;
@@ -49,6 +50,14 @@ function BookingDetail() {
         {status === "unconfirmed" && (
           <Button onClick={() => navigate(`/checkin/${bookingId}`)}>
             Check in
+          </Button>
+        )}
+        {status === "checked-in" && (
+          <Button
+            onClick={() => checkout({ bookingId })}
+            disabled={isCheckingout}
+          >
+            Check out
           </Button>
         )}
         <Button variation="secondary" onClick={moveBack}>
