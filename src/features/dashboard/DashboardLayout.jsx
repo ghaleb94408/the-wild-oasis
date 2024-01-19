3582319feb1a87bf2ee8847beba99d089b9bfd42
Ghaleb Murabyeh <ghaleb.murapyeh@gmail.com>
2024-01-19 23:31:57 +0300

    add dashboard statistics

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,8 +1,35 @@
 import styled from "styled-components";
-
+import { useRecentBookings } from "./useRecentBookings";
+import { useCabins } from "../cabins/useCabins";
+import { useRecentStays } from "./useRecentStays";
+import Spinner from "../../ui/Spinner";
+import Stats from "./Stats";
 const StyledDashboardLayout = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr 1fr 1fr;
   grid-template-rows: auto 34rem auto;
   gap: 2.4rem;
 `;
+
+export default function DashboardLayout() {
+  const { bookings, isLoading } = useRecentBookings();
+  const {
+    confirmedStays,
+    isLoading: isLoadingStays,
+    numDays,
+  } = useRecentStays();
+  const { cabins, isLoading: isLoadingCabins } = useCabins();
+  if (isLoading || isLoadingStays || isLoadingCabins) return <Spinner />;
+  return (
+    <StyledDashboardLayout>
+      <Stats
+        bookings={bookings}
+        confirmedStays={confirmedStays}
+        numDays={numDays}
+        cabinCount={cabins.length}
+      >
+        Statistics
+      </Stats>
+    </StyledDashboardLayout>
+  );
+}
