c9f8e0c056fdcfec3baab9ef482f4e821fec1b85
Ghaleb Murabyeh <ghaleb.murapyeh@gmail.com>
2024-01-21 13:28:56 +0300

    add todays activity to dashboard

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -3,6 +3,7 @@ import { useRecentBookings } from "./useRecentBookings";
 import { useCabins } from "../cabins/useCabins";
 import { useRecentStays } from "./useRecentStays";
 import Spinner from "../../ui/Spinner";
+import TodayActivity from "../check-in-out/TodayActivity";
 import Stats from "./Stats";
 import SalesChart from "./SalesChart";
 import DurationChart from "./DurationChart";
@@ -30,9 +31,9 @@ export default function DashboardLayout() {
         numDays={numDays}
         cabinCount={cabins.length}
       />
+      <TodayActivity />
       <DurationChart confirmedStays={confirmedStays} />
       <SalesChart bookings={bookings} numDays={numDays} />
-      <div>todays activity</div>
     </StyledDashboardLayout>
   );
 }
